4bcc289cc232be1cfc45375af43d283aeba100dd
unknown <patmos3433@gmail.com>
2020-05-10 02:14:13 +0900

    upload page

---
+++ server/routes/product.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { Product } = require("../models/Product");
+
+//=================================
+//             Product
+//=================================
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+var upload = multer({ storage: storage }).single("file");
+
+router.post("/image", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return req.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+router.post("/", (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+module.exports = router;
